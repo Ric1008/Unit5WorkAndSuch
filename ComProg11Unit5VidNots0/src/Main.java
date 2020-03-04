74bdead0a40de9876cd34440d29f8b49aab0013f
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2020-03-04 00:44:35 -0800

    Added
    Hopefully finish this soon.

---
+++ ComProg11Unit5VidNots0/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums={2,3,2,3};
+        String greet="Hello";
+        nums[1]=10;
+        //Modifies: this
+        //Modifies: this, deposits
+        //Effects: adds amt to balance
+
+
+
+    }
+}
