561db8d2c257ffde8833edd3d8f28f6b1cd9d47e
Ric1008 <55457271+Ric1008@users.noreply.github.com>
2020-04-13 19:29:46 -0700

    I don't know
    I don't know

---
+++ DataAbstractionAssignment/src/DepositUsage.java
@@ -0,0 +1,6 @@
+public class DepositUsage {
+    public static void main(String[] args) {
+         public double checkBalance ;
+Deposit depot=new Deposit(1,System.currentTimeMillis(),checkBalance);
+    }
+}
